9e63777977e1b7b42374e50ea2e63850df38ccb1
Alessandro Muniz <sandromuniz27@hotmail.com>
2022-02-04 19:58:45 -0400

    feat: improving Reports

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-
-import { ReportsRoutingModule } from './reports-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 
+import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 
+import { ChartModule } from 'primeng/chart';
+
 @NgModule({
+  imports: [SharedModule, ReportsRoutingModule, ChartModule],
   declarations: [ReportsComponent],
-  imports: [ReportsRoutingModule, SharedModule],
 })
 export class ReportsModule {}
