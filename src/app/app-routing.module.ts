f0ec0fc8b4ef61ddf342cdaa7d4642637d00e829
Alessandro Muniz <sandromuniz27@hotmail.com>
2022-02-05 13:42:08 -0400

    feat: updating angular version from 10 to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,26 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'entries',
-    loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule),
+    loadChildren: () =>
+      import('./pages/entries/entries.module').then((m) => m.EntriesModule),
   },
   {
     path: 'categories',
-    loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule),
+    loadChildren: () =>
+      import('./pages/categories/categories.module').then(
+        (m) => m.CategoriesModule
+      ),
   },
   {
     path: 'reports',
-    loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule),
+    loadChildren: () =>
+      import('./pages/reports/reports.module').then((m) => m.ReportsModule),
   },
   { path: '', redirectTo: '/reports', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
